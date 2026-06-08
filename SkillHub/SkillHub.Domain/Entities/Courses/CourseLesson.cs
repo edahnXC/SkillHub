5e5e86d680222c962665cdded1ce13b4fd3f9988
Om Kumar <edahnXC@gmail.com>
2026-06-08 12:31:30 +0530

    course lesson and resource added

--- SkillHub/SkillHub.Domain/Entities/Courses/CourseLesson.cs
+++ SkillHub/SkillHub.Domain/Entities/Courses/CourseLesson.cs
@@ -1,6 +1,14 @@
-﻿namespace SkillHub.SkillHub.Domain.Entities.Courses
+﻿using SkillHub.SkillHub.Domain.Common;
+
+namespace SkillHub.SkillHub.Domain.Entities.Courses;
+
+public class CourseLesson : BaseEntity
 {
-    public class CourseLesson
-    {
-    }
+    public long SectionId { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string? VideoUrl { get; set; }
+
+    public int DurationMinutes { get; set; }
 }
--- SkillHub/SkillHub.Domain/Entities/Courses/LessonResource.cs
+++ SkillHub/SkillHub.Domain/Entities/Courses/LessonResource.cs
@@ -1,6 +1,12 @@
-﻿namespace SkillHub.SkillHub.Domain.Entities.Courses
+﻿using SkillHub.SkillHub.Domain.Common;
+
+namespace SkillHub.SkillHub.Domain.Entities.Courses;
+
+public class LessonResource : BaseEntity
 {
-    public class LessonResource
-    {
-    }
+    public long LessonId { get; set; }
+
+    public string FileName { get; set; } = string.Empty;
+
+    public string FileUrl { get; set; } = string.Empty;
 }
