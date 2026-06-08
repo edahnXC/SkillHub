1351ce313ff9b8bdc2c9a593e94f319c09ea276d
Om Kumar <edahnXC@gmail.com>
2026-06-08 11:31:48 +0530

    Initial project added to github

---
+++ SkillHub/SkillHub.Domain/Entities/Identity/Role.cs
@@ -0,0 +1,11 @@
+﻿using SkillHub.SkillHub.Domain.Common;
+
+namespace SkillHub.SkillHub.Domain.Entities.Identity
+{
+    public class Role:BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ SkillHub/SkillHub.Domain/Entities/Identity/User.cs
@@ -0,0 +1,16 @@
+﻿using SkillHub.SkillHub.Domain.Common;
+
+namespace SkillHub.SkillHub.Domain.Entities.Identity
+{
+    public class User:AuditableEntity
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
+        public string? ProfileImageUrl { get; set; }
+        public bool IsEmailVerified { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
